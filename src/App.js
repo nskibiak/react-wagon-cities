efe1fe735ddb9211f53b78d727c8327370116b23
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-06 11:19:24 +0900

    now loading city image from le wagon server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ActiveCity from './containers/active_city';
 import CityList from './containers/city_list';
 import cities from './data/cities';
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className='app'>
       <CityList cities={cities} />
-      <div class="active-city"></div>
+      <ActiveCity city={cities[0]} />
     </div>
   );
 }
---
+++ src/containers/active_city.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ActiveCity extends Component {
+  render() {
+    let image = `https://kitt.lewagon.com/placeholder/cities/${this.props.city.slug}`
+
+    return(
+      <div className="active-city">
+        <h3>{this.props.city.name}</h3>
+        <img src={image} alt=""/>
+      </div>
+    )
+  };
+};
+
+export default ActiveCity;
