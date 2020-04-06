1aba7ae3c730d7ce8e92aa5f5939eaa1d056929e
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-06 10:55:56 +0900

    cleaned up create-react-app template docs, got app working with city_list and city containers

---
+++ src/containers/city.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class City extends Component {
+  render() {
+    return (
+      <p>is this thing on?</p>
+    )
+  }
+};
+
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import City from './city';
+
+class CityList extends Component {
+  render() {
+    return <City />
+  };
+};
+
+export default CityList;
